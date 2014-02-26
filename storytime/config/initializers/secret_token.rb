a5fa693d55d5402080d7e45a8dcd98f77dcb2930
taborke <kevin.tabor@gmail.com>
2014-02-26 11:09:16 -0600

    full upload

---
+++ storytime/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storytime::Application.config.secret_key_base = '3e67b76e9166cc782fb5890e95580c1d821c201fc7f395b4d6b045970e974a754fbd869e39508babf48e81651d831df5fbd5251784015e5ce270030353b6b135'
---
+++ storytime/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Storytime::Application.config.session_store :cookie_store, key: '_storytime_session'
