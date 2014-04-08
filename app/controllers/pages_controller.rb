8f5ab12a4b25868d07d47dd4e842822e58e2cddf
dorthwein <dorthwein@gmail.com>
2014-04-08 08:43:10 -0500

    Adding New Story Page functionallity

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,7 @@ class PagesController < ApplicationController
 
   # GET /pages/new
   def new
+    @story = Story.find(params[:story_id])
     @page = Page.new
   end
 
@@ -31,7 +32,7 @@ class PagesController < ApplicationController
 
     respond_to do |format|
       if @page.save
-        format.html { redirect_to [@story, @page], notice: 'Page was successfully created.' }
+        format.html { redirect_to @story, notice: 'Page was successfully created.' }
         format.json { render action: 'show', status: :created, location: @page }
       else
         format.html { render action: 'new' }
